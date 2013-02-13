62f5e1f75aab98976fa5cc71b4ac67e1b563e0bc
Christina Hang <christinah0113@gmail.com>
2013-02-12 18:16:12 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoginCounter::Application.config.secret_token = 'f13bd6c77d32ac6ab7dd71f111044f1b31afe8117fde992f842cbefbcb9e9b98c70cc6bba3cde29dd9876ba68a905f97b7d6b2f5534a11546e0587d7c4bf8edc'
